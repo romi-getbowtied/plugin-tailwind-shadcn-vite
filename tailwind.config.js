2cc8742e0a18c8f32a18b55a3d82e6fc0586de14
Romi H. <romi@getbowtied.com>
2025-12-06 14:44:46 +0200

    ...

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export default {
+  content: [
+    './src/**/*.{ts,tsx,js,jsx}',
+    './admin/**/*.php',
+  ],
+  important: '#my-plugin-app',
+};
+
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react({ jsxRuntime: "classic" })],
+  plugins: [
+    react({ jsxRuntime: "classic" }),
+    tailwindcss({
+      base: "./src/index.css",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
