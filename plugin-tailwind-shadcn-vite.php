b04ff529831d32072f772cc7741d3fc6ee061e20
Romi H. <romi@getbowtied.com>
2025-12-11 10:42:47 +0200

    ...

--- plugin-tailwind-shadcn-vite.php
+++ plugin-tailwind-shadcn-vite.php
@@ -45,7 +45,6 @@ class Tailwind_Scoped_Plugin {
 						<?php GBT_Component_Nav_Menu::render('primary'); ?>
 					</div>
 				</div>
-				<?php GBT_Component_Hero_Parallax::render(); ?>
 				<div class="p-6 space-y-6">
 					<div class="flex flex-wrap gap-4 pt-4">
 						<div class="flex gap-2">
